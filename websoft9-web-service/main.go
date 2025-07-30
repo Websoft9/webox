5e8d714025200c10cf1ba7c56eaeb02683986385
qiaofeng1227 <76487013@qq.com>
2025-07-30 15:45:27 +0800

    main edit

--- websoft9-web-service/main.go
+++ websoft9-web-service/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 	"websoft9-web-service/internal/config"
 	"websoft9-web-service/internal/database"
 	"websoft9-web-service/internal/router"
 	"websoft9-web-service/internal/service"
-
-	"github.com/go-redis/redis/v9"
-	"golang.org/x/net/context"
 )
 
 func main() {
@@ -31,31 +26,11 @@ func main() {
 	// 	log.Fatal("Failed to migrate database:", err)
 	// }
 
-	// 从环境变量读取 Redis 配置
-	redisHost := os.Getenv("REDIS_HOST")
-	redisPort := os.Getenv("REDIS_PORT")
-	if redisHost == "" || redisPort == "" {
-		fmt.Println("REDIS_HOST or REDIS_PORT is not set")
-		os.Exit(1)
-	}
-
-	// 构建 Redis 地址
-	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
-
-	// 初始化 Redis 客户端
-	rdb := redis.NewClient(&redis.Options{
-		Addr: redisAddr,
-	})
-
-	// 测试连接
-	ctx := context.Background()
-	_, err = rdb.Ping(ctx).Result()
+	// 初始化Redis
+	rdb, err := database.InitRedis(cfg)
 	if err != nil {
-		fmt.Printf("Failed to connect to Redis: %v\n", err)
-		os.Exit(1)
+		log.Fatal("Failed to initialize Redis:", err)
 	}
-
-	fmt.Println("Connected to Redis successfully!")
 
 	// 初始化InfluxDB
 	influxClient, err := database.InitInfluxDB(cfg)
